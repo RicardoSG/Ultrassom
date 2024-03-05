6868a034685e8c45e17f84c7d2df6bbc1f59cd6e
Ricardo Schmidt Gonçalves <ricardo.tyrael@gmail.com>
2024-03-04 22:41:23 -0300

    ultrasson read in inches and cm

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,40 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+int trigger = 2;
+int echo = 3;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+ 
+  Serial.begin(9600);
+  pinMode(trigger, OUTPUT);
+  pinMode(echo, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+  long duration, inches, cm;
+
+  digitalWrite(trigger, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigger, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(trigger, LOW);
+
+  //pulseIn is a function to read a pulse duration
+  duration = pulseIn(echo, HIGH);
+
+  inches = duration / 74 / 2;
+  cm = duration / 29 / 2;
+
+  Serial.print("inches: ");
+  Serial.print(inches);
+  Serial.print(" cm: ");
+  Serial.print(cm);
+  
+  Serial.println();
+
+  delay(100);
+  
+
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
